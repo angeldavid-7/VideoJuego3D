4c2b1dd2ca575ed120c62d38a9db59d7e66a3929
angeldavid-7 <aurregol@ucentral.edu.co>
2021-05-14 17:43:59 -0500

    pantalla de incio y prototipo de menu
    Cree una pantalla con mucho texto y un prototipo del menu, (el menu nesesita cambios artisticos)

---
+++ Assets/script/LevelMgr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class LevelMgr : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargaNivel(string pNombreNivel)
+    {
+        SceneManager.LoadScene(pNombreNivel);
+        
+    }
+
+    public void CerrarJuego()
+    {
+        Application.Quit();
+        Debug.Log("Funciona Salir");
+    }
+
+
+}
---
+++ Assets/script/Puerta.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+    public float speed = 0;
+
+    public Vector3 direction;
+
+
+    public bool puedeAbrir;
+
+    void Moverse()
+    {
+
+        gameObject.transform.Translate(0, 0, 0);
+
+    }
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (puedeAbrir == true )
+        {
+           
+                
+                Moverse();
+                
+            
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+#pragma warning disable CS0642 // Posible instrucción vacía errónea
+        if (other.gameObject.tag == "player") ;
+#pragma warning restore CS0642 // Posible instrucción vacía errónea
+        
+       PresionarTab.aparecer = true;
+        puedeAbrir = true;
+    }
+
+     void OnTriggerExit(Collider other)
+    {
+#pragma warning disable CS0642 // Posible instrucción vacía errónea
+        if (other.gameObject.tag == "player") ;
+#pragma warning restore CS0642 // Posible instrucción vacía errónea
+        PresionarTab.aparecer = false;
+        puedeAbrir = false;
+        
+    }
+}
+//(0.34375f, 2.6f, 9)
